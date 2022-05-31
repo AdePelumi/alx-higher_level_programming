2fffeb6cc4d9df4cbbf3ec3e8c69c5383d77ab39
AdePelumi <lordbatech00@gmail.com>
2022-05-31 10:57:09 -0700

    changed from f strings to string formatter

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for b in range(90, 64 , -1):
-    print(chr(b + 32) if b % 2 == 0 else chr(b),end="")
+for b in range(90, 64, -1):
+    print("{}".format(chr(b+32))\
+            if b % 2 == 0 else "{}".format(chr(b)), end="")
